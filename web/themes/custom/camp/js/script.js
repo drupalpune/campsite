6caec68d442bd6ec1bc29093364bbea2aab70afa
digitalpolygon.sibu.stephen <sibu.stephen@digitalpolygon.com>
2023-05-30 19:25:23 +0530

    Menu fixes.

--- web/themes/custom/camp/js/script.js
+++ web/themes/custom/camp/js/script.js
@@ -1,17 +1,17 @@
 (function ($, Drupal) {
     Drupal.behaviors.headerScroll = {
       attach: function (context, settings) {
-
-        $(document).ready(function() {
+        $(document).ready(function(e) {
           $(context).find('.profile-name').on('click', function() {
             console.log('Clicked on .profile-name');
             $(this).toggleClass('open');
             console.log($(this).next('.profile-dropdown-menu')); // Check if the element is selected
             $(this).next('.profile-dropdown-menu').toggleClass('open');
           });
+          // Appending the Register now Button and Profile name ot Hamburger menu.
+          $(context).find('.profile-name').length?$(context).find('.profile-name').clone().appendTo($(context).find('.cheeseburger-menu__mainmenu')):'';
+          $(context).find('.register-now').length? $(context).find('.register-now').clone().appendTo($(context).find('.cheeseburger-menu__mainmenu')): '';   
         });
-        
-
         
         $(window, context).on('scroll', function () {
           var header = $('.page-header');
